01ba0b2c16a15783a75dd1a861fd029363963c8f
rohhyungwoo <67363759+rohhyungwoo@users.noreply.github.com>
2023-03-31 23:35:39 +0900

    3-31

--- CodeLabRoom/app/src/main/java/com/example/codelabroom/MainActivity.kt
+++ CodeLabRoom/app/src/main/java/com/example/codelabroom/MainActivity.kt
@@ -30,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
+
         // Add an observer on the LiveData returned by getAlphabetizedWords.
         // The onChanged() method fires when the observed data changes and the activity is
         // in the foreground.
